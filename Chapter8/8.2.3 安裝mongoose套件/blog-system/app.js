127aca6948a4d6900a8ef037021e8f1eff96a204
WesleyTsai <wesleyboy42@gmail.com>
2015-08-14 15:58:23 +0800

    Update the app.js to meet book's example for chapter 8.2.3.

--- Chapter8/8.2.3 安裝mongoose套件/blog-system/app.js
+++ Chapter8/8.2.3 安裝mongoose套件/blog-system/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 啟用cookieSession
+app.use(cookieSession({
+    key: 'node',
+    secret: 'HelloExpressSESSION'
+}))
 
 app.use('/', routes);
 app.use('/users', users);
